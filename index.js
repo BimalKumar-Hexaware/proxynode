633e1290adb482365b820bea2d2c047bc2d19314
Bimal Kumar <40321@hexaware.com>
2018-07-18 10:55:48 +0530

    cc

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 'use strict';
-const express = require('express')
-const bodyParser = require('body-parser');
-const app = express();
-const request = require('request');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+var httpProxy = require('http-proxy');
 const REST_PORT = process.env.PORT || 8081;
 
-
-app.post('/api', (req, res) => {
-    console.log('Assistant req body: ' + JSON.stringify(req.body));
-    var options = {
-        proxy: 'http://gmdvproxy.acml.com:8080/',
-        method: 'POST',
-        url: 'http://10.82.184.75.:8880',
-        headers:
-        {
-            'content-type': 'application/json',
-        },
-        //body:req.body
-    };
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        }
-        console.log(body);
-    });
-});
-
-app.listen(REST_PORT, function () {
+httpProxy.createServer({
+    target: {
+        port: 8880,
+        host: 'http://10.82.184.75'
+    },
+    forward: {
+        port: 8080,
+        host: 'http://gmdvproxy.acml.com'
+    }
+}).listen(REST_PORT, function () {
     console.log('Rest service ready on port ' + REST_PORT);
 });
