d45f86753faa94b486ee66dd2d94bb5a83a0747f
Bimal Kumar <40321@hexaware.com>
2018-07-18 11:02:27 +0530

    sss

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const REST_PORT = process.env.PORT || 8081;
 
 httpProxy.createServer({
     target: {
-        port: 8880,
-        host: 'http://10.82.184.75'
-    },
-    forward: {
+        
         port: 8080,
         host: 'http://gmdvproxy.acml.com'
+    },
+    forward: {
+        port: 8880,
+        host: 'http://10.82.184.75'
     }
 }).listen(REST_PORT, function () {
     console.log('Rest service ready on port ' + REST_PORT);
