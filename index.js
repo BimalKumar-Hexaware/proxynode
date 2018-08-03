7eb949ad2e68408c3f49971e11f2511adbc2519c
Bimal Kumar <40321@hexaware.com>
2018-08-03 15:11:00 +0530

    dd

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var httpProxy = require('http-proxy');
 var port = process.env.PORT || 3214; 
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
+
+var proxy = httpProxy.createProxyServer({target:'https://abvisit-poc.herokuapp.com'}).listen(port, function () {
+	console.log("Application started listening port " + port);
+});
 
 proxy.on('proxyRes', function (proxyRes, req, res) {
   console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
 });
 
 proxy.on('close', function (res, socket, head) {
-  // view disconnected websocket connections
   console.log('Client disconnected');
 });
  
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, { target: 'https://abvisit-poc.herokuapp.com' });
-});
- 
-console.log("proxy listening on port "+port)
-server.listen(port);
+
