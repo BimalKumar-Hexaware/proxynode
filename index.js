8954779bf5a1703be7b14249ee1f995e21532e04
Bimal Kumar <40321@hexaware.com>
2018-08-03 13:33:55 +0530

    hh

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ var proxy = httpProxy.createProxyServer({});
 // a web request to the target passed in the options
 // also you can use `proxy.ws()` to proxy a websockets request
 //
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+});
+
 var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
